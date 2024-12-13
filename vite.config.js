79851667fc112bfbfcc8b4da78beece7fe3a5443
Mahimna M Mehta <65457672+mahimnamehta@users.noreply.github.com>
2024-12-13 16:41:03 -0500

    updated build settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping",
+  base: "./e-plantShopping",
   plugins: [react()],
+  build: {
+    outDir: './build',
+  }
 })
